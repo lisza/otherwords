782704c081a0a1cd3fc59ee1b80ddef96e687df7
lisza <liszalisza@gmail.com>
2017-10-03 23:14:53 -0700

    Get Google Translate Api batch requests working

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
+var path = require('path');
+
 module.exports = {
-    entry: "./lib/main.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    devtool: "source-map",
-    resolve: {
-    extensions: [".js", ".json", "*"]
+  entry: "./lib/entry.js",
+  output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: "bundle.js"
   },
-    target: "node"
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  }
 };
